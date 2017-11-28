04a7fe537f837e7509298f082db5779bd168d02a
carlosg <carlosenrique.gomezgomez@telefonica.com>
2017-11-28 11:30:59 +0100

    fix

--- src/rpm/SOURCES/opt/ss/develenv/platform/devpi-server/bin/init-devpi.sh
+++ src/rpm/SOURCES/opt/ss/develenv/platform/devpi-server/bin/init-devpi.sh
@@ -3,8 +3,8 @@ source /etc/sysconfig/develenv-devpi
 echo "[INFO] Initializing devpi repository"
 PYTHON_VERSION=$(python --version 2>&1|awk '{print $2}'|cut -d'.' -f1,2) 
 export PYTHONPATH=$DEVPI_HOME/lib/python${PYTHON_VERSION}/site-packages/
-DEVPI_COMMAND="${DEVPI_HOME_CLIENT}/bin/devpi"
-$DEVPI_COMMAND use http://localhost/devpi
+DEVPI_COMMAND="${DEVPI_CLIENT_HOME}/bin/devpi"
+$DEVPI_COMMAND use http://localhost:${DEVPI_LISTEN_PORT}
 $DEVPI_COMMAND login root --password ''
 $DEVPI_COMMAND user -m root password=temporal
 $DEVPI_COMMAND logoff
